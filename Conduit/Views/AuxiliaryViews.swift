c164aaa7a3743a9b30ef7d1dd47e2ca67d700f75
Ryan Weddle <weddle@gmail.com>
2026-08-09 13:26:15 -0400

    Fix haptics toggle persistence

--- Conduit/Views/AuxiliaryViews.swift
+++ Conduit/Views/AuxiliaryViews.swift
@@ -798,7 +798,6 @@ private struct ChatReturnBehaviorSettings: View {
         persist(next)
     }
 }
-
 private struct ResponseBehaviorSettings: View {
     let initialMode: BusyInputMode
     let save: (BusyInputMode) async -> Bool
